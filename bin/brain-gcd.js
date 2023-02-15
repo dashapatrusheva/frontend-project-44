009d111f1c76ba71fee2c79eb6b03d0159fe3895
dashapatrusheva <dpatrushheva@yandex.ru>
2023-02-15 13:01:48 +0300

    for tests5

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import gameGcb from '../src/games/gcd.js';
 
 gameGcb();
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import gameProgression from '../src/games/progression.js';
 
 gameProgression();
